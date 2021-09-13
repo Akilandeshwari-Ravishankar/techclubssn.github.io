4d627d57277ae8fc4d6f32ca50c98b461e16c72c
Lalith <lalithleftie@gmail.com>
2021-09-13 20:48:11 +0530

    initial commit

---
+++ build/precache-manifest.047bffc721a3fcea06d1914d6a6329fb.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "bb090b9efe2d63379b0d",
+    "url": "/static/css/main.3602a457.chunk.css"
+  },
+  {
+    "revision": "bb090b9efe2d63379b0d",
+    "url": "/static/js/main.bb090b9e.chunk.js"
+  },
+  {
+    "revision": "5f9ba3debd1a86c2ccc9",
+    "url": "/static/js/1.5f9ba3de.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "aaf89485e7e17f1b58479a0b3aa6ded7",
+    "url": "/static/media/agritech.aaf89485.svg"
+  },
+  {
+    "revision": "1bb9a4ecbecda8d25022ed679f190af2",
+    "url": "/static/media/climate.1bb9a4ec.svg"
+  },
+  {
+    "revision": "4d349fc7cefacd3e5d405bf600b681af",
+    "url": "/static/media/cyber.4d349fc7.svg"
+  },
+  {
+    "revision": "aea4fb65fafecd4a600623561220dceb",
+    "url": "/static/media/fintech.aea4fb65.svg"
+  },
+  {
+    "revision": "2209f7951259426c376c11b8325fb049",
+    "url": "/static/media/innovation.2209f795.svg"
+  },
+  {
+    "revision": "e9cf4e4ec1f5641249cd82836521cbc8",
+    "url": "/static/media/HealthcareIcon.e9cf4e4e.svg"
+  },
+  {
+    "revision": "5684b30b741afff6ef5edde8abacd6de",
+    "url": "/static/media/women.5684b30b.svg"
+  },
+  {
+    "revision": "4387a5d11335d957a0e7e9728843d368",
+    "url": "/static/media/phone.4387a5d1.svg"
+  },
+  {
+    "revision": "bc0e99cac60763d4de00615144264032",
+    "url": "/static/media/person.bc0e99ca.svg"
+  },
+  {
+    "revision": "ddc7c9227d501f22b5e4b017aa796f59",
+    "url": "/static/media/calendar.ddc7c922.svg"
+  },
+  {
+    "revision": "e063e198e757693229d856a1e9ac9620",
+    "url": "/index.html"
+  }
+];
